39452e98e6169a6e2aa4d1a32fc3fb19abb4a438
Vladimir S. Medvedev <stalker1984@mail.ru>
2023-09-15 13:52:48 +0300

    minor solve problem

--- django_app/main/models.py
+++ django_app/main/models.py
@@ -1,8 +1,11 @@
-from django.db import models
+#from django.db import models
 from django.contrib.auth.models import User
-
+'''
 class Person(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     company_name = models.CharField(max_length=100)
     full_addres = models.CharField(max_length=100)
 
+    def __str__(self) -> str:
+        return f"Person: {self.company_name}"
+'''
