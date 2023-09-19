9e3da84369f92177f633020cec9f298d38e3a49a
Vladimir S. Medvedev <stalker1984@mail.ru>
2023-09-19 10:38:43 +0300

    no commit

--- django_app/main/models.py
+++ django_app/main/models.py
@@ -1,6 +1,7 @@
-#from django.db import models
+from django.db import models
 from django.contrib.auth.models import User
-'''
+
+
 class Person(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     company_name = models.CharField(max_length=100)
@@ -8,4 +9,3 @@ class Person(models.Model):
 
     def __str__(self) -> str:
         return f"Person: {self.company_name}"
-'''
